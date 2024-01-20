376542a7d47a720ef07ff891bddb8836e6e9a769
Darshaka lanka <darshaka@futuralabs.tech>
2024-01-20 12:00:15 +0530

    update routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
     // loader: rootLoader,
     children: [
       {
-        path: "chat/:name",
+        path: "/chat/:name",
         element: <Chat />,
         // loader: teamLoader,
       },
